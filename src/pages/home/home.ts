ff3d9a520364b4a2cdbdf235c7bc47d1fa52271c
Ritumetse <ntshechengpogisho@gmail.com>
2018-10-23 10:42:26 +0200

    initial commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -8,41 +8,42 @@ selector: 'page-home',
 templateUrl: 'home.html'
 })
 export class HomePage {
-weather:any;
-city: string;
-value: string;
-humidity: string;
-wspeed: string;
-main: string;
-description: string;
-wind: string;
-Temperature: number;
-date: number;
+  weather:any;
+  humidity:number;
+  main:string;
+  date:number;
+  Temp:number;
+  city:string;
+  windspeed:number;
+  wind:number;
+  description:number;
+  id:number;
+  icon:string;
+  location:string
+  temp_max:number;
+  temp_min:number;
 
 
 constructor(public navCtrl: NavController, private weatherP: WeatherProvider) {
 
 }
-
-getWheather(value:string) {
-  this.weatherP.getWeather(this.city).subscribe(results => {
-  this.weather = results;
-
-
-    this.main = this.weather.weather[0].main;
-    this.description = this.weather.weather[0].description;
-    this.humidity=this.weather.main.humidity;
-    this.Temperature = this.weather.main.temp-273.15;
-
-    this.wind = this.weather.wind.speed;
-  
-    this.date = this.weather.dt;
-    console.log("response", results);
-
-
-  });
-}
-}
+getWeather(cvalue:any){
+  this.weatherP.getWeather(this.city).subscribe(results=>{
+   this.weather=results;
+   this.main=this.weather.weather[0].main;
+   this.humidity=this.weather.main.humidity;
+   this.Temp=this.weather.main.temp-273.15;
+   this.date=this.weather.dt;
+   this.wind=this.weather.wind.speed;
+   this.description=this.weather.weather[0].description;
+   this.id=this.weather.weather[0].id;
+   this.location=this.weather.weather[0].city
+   this.temp_max=this.weather.main.temp_max-273.15;
+   this.temp_min=this.weather.main.temp_min-273.15;
+   console.log("response",results)
+  });                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        
+  }
+ }
 
 
 
